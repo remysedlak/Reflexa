f626d30e92572b5c679bc2ad87f3e142e4329726
remysedlak <remysedlak@gmail.com>
2025-01-11 17:46:58 -0500

    Fix URL path for mood colors API endpoint in urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -5,7 +5,7 @@ from app.views import PastThirtyDaysInsights, MonthlyInsightsView, DaysItemView,
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/days/', DaysItemView.as_view(), name="days"),
-    path('apil/mood-colors/', mood_colors, name='mood-colors'),
+    path('api/mood-colors/', mood_colors, name='mood-colors'),
     path('api/past-thirty-days/', PastThirtyDaysInsights.as_view(), name='past-thirty-days'),
     path('api/aggregated-data/', AggregatedDataView.as_view(), name='aggregated-data'),
     path('api/monthly-insights/<int:month>/<int:year>/', MonthlyInsightsView.as_view(), name='monthly_insights'),
