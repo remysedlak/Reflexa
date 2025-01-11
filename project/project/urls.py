138912adb6726414a9c47a26f637fa84c28a8a38
remysedlak <remysedlak@gmail.com>
2025-01-11 15:00:32 -0500

    urles

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import DaysItemView, mood_colors, DayDeleteView
+from app.views import DaysItemView, mood_colors, DayDeleteView, AggregatedDataView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
